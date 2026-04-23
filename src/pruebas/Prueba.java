28c58ad50ef5efd10eb0368ceb58ff630421deb1
JorgePerez19 <46350973+JorgePerez19@users.noreply.github.com>
2026-04-23 15:23:07 -0500

    Update Prueba.java

--- src/pruebas/Prueba.java
+++ src/pruebas/Prueba.java
@@ -12,5 +12,6 @@ public class Prueba {
       public static void main(String[] args) {
         // TODO code application logic here
           System.out.println("Hola mundo");
+            System.out.println("Hola mundo 3");
     }
 }
